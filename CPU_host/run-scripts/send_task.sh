395f3c1d79c5d74916af0f67cf7f931403013dd0
gary9630 <gary96302000.eecs96@g2.nctu.edu.tw>
2020-12-27 17:53:36 +0800

    Update path of sending task

--- CPU_host/run-scripts/send_task.sh
+++ CPU_host/run-scripts/send_task.sh
@@ -2,8 +2,8 @@
 cat /root/.ssh/google_compute_engine
 cat /root/.ssh/google_compute_engine.pub
 
-gcloud compute scp ./tasks/task.csv $USER@$GPU_INSTANCE_NAME:~/tasks/test.csv --zone=$GPU_INSTANCE_ZONE --ssh-key-file=/root/.ssh/google_compute_engine
+gcloud compute scp ./tasks/task.csv $USER@$GPU_INSTANCE_NAME:~/rumors-ai-bert/GPU_host/tasks/test.csv --zone=$GPU_INSTANCE_ZONE --ssh-key-file=/root/.ssh/google_compute_engine
 
-gcloud compute ssh $USER@$GPU_INSTANCE_NAME --zone $GPU_INSTANCE_ZONE --command="bash predict_ModelA.sh"
+gcloud compute ssh $USER@$GPU_INSTANCE_NAME --zone $GPU_INSTANCE_ZONE --command="bash ~/rumors-ai-bert/GPU_host/predict_ModelA.sh"
 
-gcloud compute scp $USER@$GPU_INSTANCE_NAME:~/prediction/Predict_results.txt ./tasks/result_task.txt  --zone $GPU_INSTANCE_ZONE --ssh-key-file=/root/.ssh/google_compute_engine
+gcloud compute scp $USER@$GPU_INSTANCE_NAME:~/rumors-ai-bert/GPU_host/prediction/Predict_results.txt ./tasks/result_task.txt  --zone $GPU_INSTANCE_ZONE --ssh-key-file=/root/.ssh/google_compute_engine
