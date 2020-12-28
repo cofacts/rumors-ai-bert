a4a1f0cd2914eacc2b9be4ff3857ef9454dacd2e
gary9630 <gary96302000.eecs96@g2.nctu.edu.tw>
2020-12-28 23:12:02 +0800

    Update some paths of script files

--- GPU_host/predict_ModelA.sh
+++ GPU_host/predict_ModelA.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-rm ./tasks/cached*
+rm ~/rumors-ai-bert/GPU_host/tasks/cached*
 
 /opt/conda/bin/python ~/rumors-ai-bert/GPU_host/model_bert/run_multi_label_classification.py \
 	--task_name cofacts \
 	--model_name_or_path models_bert \
 	--do_eval \
-	--data_dir ./tasks/ \
+	--data_dir ~/rumors-ai-bert/GPU_host/tasks/ \
 	--predict_file test.csv \
-	--output_dir ./prediction/ \
+	--output_dir ~/rumors-ai-bert/GPU_host/prediction/ \
 	--per_gpu_eval_batch_size=16 \
--- GPU_host/train_ModelA.sh
+++ GPU_host/train_ModelA.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-python ./model_bert/run_multi_label_classification.py \
+python ~/rumors-ai-bert/GPU_host/model_bert/run_multi_label_classification.py \
 	--task_name cofacts \
 	--model_name_or_path bert-base-chinese \
 	--do_train \
 	--do_eval \
-	--data_dir ./rumors-ai/ai_model/data/processed_data/ \
+	--data_dir ~/rumors-ai-bert/GPU_host/data/processed_data/ \
 	--learning_rate 1e-4 \
 	--num_train_epochs 3 \
 	--max_seq_length 128 \
